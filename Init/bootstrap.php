635701a438d75d43eb84639e3074560267290efb
Fano <fano.develops@gmail.com>
2025-08-06 00:51:29 +0300

    enhance struct:

---
+++ Init/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *  ATTENTION : Le nom du fichier n'a rien à voir
+ *  avec Bootstrap framework Css
+ * 
+ *  Bootstrap signifie : "qui met en place", "qui initialise"
+ *  "qui configure", "qui prépare", ...
+ */
+
+require_once __DIR__ . "/myautoload.php";
--- index.php
+++ index.php
@@ -1,3 +1,2 @@
 <?php
-include_once "./myautoload.php";
-
+require_once __DIR__ . "/init/bootstrap.php";
